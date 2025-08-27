0f256e2e780917c9bfd47b7bfaef5a12a2aeb92d
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-08-27 16:25:45 +0700

    all

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // <- ini yang bikin build jalan meskipun ada error ESLint
+  },
 };
 
 export default nextConfig;
